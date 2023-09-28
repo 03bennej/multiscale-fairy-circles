6dcb323b10ee9087aaa05bbd9269cead69cd893d
03bennej <jjrbenn@gmail.com>
2023-09-28 12:50:38 +0000

    config files

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages
+
+setup(name='src',       # name of package
+      version='0.0',    # version of package
+      packages=find_packages())     # looks for all __init__.py files in directory
